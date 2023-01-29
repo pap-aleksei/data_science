21c142db8a4c400ce81b5e7fc0bf39847f1f6e37
pap-aleksei <109242678+pap-aleksei@users.noreply.github.com>
2023-01-29 22:56:53 +0300

    Cleaned_commit

--- DS/numpy_arrays.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def get_chess(a):
-    arr = np.zeros((a, a))
-    arr[1:,0::2][::2] = 1
-    arr[:,1::2][::2] = 1
-    return arr
-
-print(get_chess(8))
--- DS/numpy_test.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-a = -456
-a = np.uint8(a)
-print(a)
